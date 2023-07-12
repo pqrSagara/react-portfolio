d3082b480deec14b3721b2e06f686cb3f838d258
Sagara <karlosagara@gmail.com>
2023-07-13 03:27:20 +0800

    portfolio added

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
 import './portfolio.css'
+import portfolio1 from '../../assets/portfolio1.png'
+import portfolio2 from '../../assets/portfolio2.png'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h1>Portfolio</h1>
+      <div className='container portfolio__container'>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={portfolio2} alt="" />
+          </div>
+          <h3>Lyceum Online Completion</h3>
+          <div className="a">
+            <p>An online web app for processing late exam completion process</p>
+          </div>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={portfolio1} alt="" />
+          </div>
+          <h3>Project title</h3>
+          <div className="a"></div>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={portfolio1} alt="" />
+          </div>
+          <h3>Project title</h3>
+          <div className="p"></div>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={portfolio1} alt="" />
+          </div>
+          <h3>Project title</h3>
+          <div className="p"></div>
+        </article>
+      </div>
+    </section>
   )
 }
 
